8da133f0e6889e0b8a1e7ae0dfa26d4e42ad3125
Higor Ribeiro Araujo <higorribeiroaraujo007@gmail.com>
2024-11-27 10:46:08 -0300

    feat: tela principal com dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -50,7 +50,15 @@
     text: 'Produtos',
     value: 'products'
   }];
-  const panel = ref([0, 1]);
+  const value = [
+    423,
+    446,
+    675,
+    510,
+    590,
+    610,
+    760,
+  ];
   const readonly = ref(false);
   const deposits: Deposit[] = [
     {
@@ -357,7 +365,43 @@
                 <v-tabs-window-item 
                   value="main"
                 >
-                    One
+                  <div class="area-dashboard d-flex flex-lg-row flex-column ga-10">
+                    <div class="w-lg-50 w-sm-100">
+                      <v-card
+                        class="mx-auto text-center"
+                        color="white"
+                        elevation="0"
+                      >
+                        <v-card-text class="pa-0">
+                          <div class="text-lg-h4 text-sm-h5 font-weight-thin text-left">
+                            Vendas nas últimas 24 horas
+                          </div>
+                        </v-card-text>  
+                        <v-card-text class="pa-0">
+                          <v-sheet
+                            class="rounded-lg border-sm" 
+                            color="white"
+                          >
+                            <v-sparkline
+                              :model-value="value"
+                              color="#00c6ff"
+                              height="170"
+                              padding="24"
+                              stroke-linecap="round"
+                              smooth
+                            >
+                              <template v-slot:label="item">
+                                R${{ item.value }}
+                              </template>
+                            </v-sparkline>
+                          </v-sheet>
+                        </v-card-text>
+                      </v-card>
+                    </div>
+                    <div class="area-map w-lg-50 w-sm-100">
+
+                    </div>
+                  </div>
                 </v-tabs-window-item>
 
                 <v-tabs-window-item value="deposits">
@@ -365,7 +409,7 @@
                     <v-col
                       cols="lg:6 md:6 sm:12"
                     >
-                      <div class="area-map">
+                      <div class="area-map" style="height: 580px;">
 
                       </div>
                     </v-col>
@@ -478,13 +522,17 @@
     padding: 23px 33px;
   }
 
+  .area-dashboard {
+    max-height: 580px;
+  }
+
   .logo {
     width: 207px;
     height: 68px;
   }
 
   .area-map {
-    height: 556px;
+    min-height: 300px;
     border-radius: 10px;
     background-color: aqua;
   }
